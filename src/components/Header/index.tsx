30e3974947487ad1a0167decf4c3646cd2238b1c
Giovani Lamastra <giovanilamastra@gmail.com>
2021-04-20 23:51:39 -0300

    api

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+import styles from "./styles.module.scss";
+
+const Header = () => {
+  const currentDate = format(
+    new Date(),
+    "EEEEEE, d MMMM ",
+    {
+      locale: ptBR,
+    }
+  );
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="" />
+      <p>O melhor para você ouvir, sempre</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+};
+
+export default Header;
